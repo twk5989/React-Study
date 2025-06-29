b0a2ed0a4788b585900ca0749fb89bd81ae6c3d4
twk5989 <a37225989@gmail.com>
2025-06-30 00:34:29 +0900

    css 변경

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import React from "react";
+import "../assets/styles/Board.css";
 
 const Board = ({ memoList }) => {
   return (
@@ -20,61 +21,3 @@ const Board = ({ memoList }) => {
 };
 
 export default Board;
-
-
-const MemoWrapper = styled.div`
-  position: relative;
-  display: flex;
-  gap: 16px;
-  flex-wrap: wrap;
-  padding: 16px;
-  width: 800px;
-  margin: 0 auto;
-  min-height: 240px;
-  height: 450px;
-  overflow-y: scroll;
-  align-items: flex-start;
-  justify-content: center;
-  border: 14px solid #875a36;
-  background-color: #81b771;
-`;
-
-const MemoCard = styled.div`
-  width: 220px;
-  min-height: 150px;
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 12px;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
-  h2 {
-    font-size: 18px;
-    margin-bottom: 4px;
-    color: #333;
-  }
-  .date {
-    font-size: 14px;
-    color: #777;
-    margin-bottom: 8px;
-  }
-  p {
-    font-size: 15px;
-    color: #555;
-  }
-`;
-
-const LOL = styled.span`
-  position: absolute;
-  font-size: 24px;
-  font-weight: 700;
-  color: #fff;
-  bottom: 50px;
-  right: 16px;
-  transform: rotate(-8deg);
-`;
-
-const EmptyMessage = styled.div`
-  font-size: 18px;
-  color: white;
-  text-align: center;
-  margin-top: 100px;
-`;
