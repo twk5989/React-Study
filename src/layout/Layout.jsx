38e0abec7ce5ab799d159a149ca6523ed96fd3d6
twk5989 <a37225989@gmail.com>
2025-07-07 20:23:16 +0900

    reouter 분리. 원래는 이렇게 해야함

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,9 @@
+//여기에 헤더와 푸터를 정의하고
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+
+
+
+export default Layout;
---
+++ src/router/index.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "../layout/Layout";
+import Home from "../pages/Home";
+import Add from "../pages/Add_page";
+import Detail from "../pages/Detail_page";
+import Login from "../pages/Login";
+
+// createBrowserRouter 사용 (권장 방식)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,  // 공통 레이아웃 (Header + Footer)
+    children: [
+      { path: "", element: <Home /> },
+      { path: "add", element: <Add /> },
+      { path: "login", element: <Login /> },
+      { path: "detail", element: <Detail /> },
+    ],
+  },
+]);
+
+export default router;
